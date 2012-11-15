521515c65a0237612e6fa6c18a3f20c7d39d7cac
Mika Attila <radiantstatue@gmail.com>
2012-11-15 12:50:29 +0100

    Convert code to standard c90
    - Make the comment a c90 comment ( /* */ )
    - Move declarations to top of scope

--- main.c
+++ main.c
@@ -5,18 +5,22 @@
 
 int main()
 {
-    srand(time(NULL));
     char password[17];
     const char *legalcharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     int i;
+    FILE* file;
+    
+    srand(time(NULL));
+    
     for(i = 0; i < 16; i++)
     {
         password[i] = legalcharacters[rand() % strlen(legalcharacters)];
     }
+    
     password[16] = '\0';
 
-    //Write to File
-    FILE *file = fopen("password.txt", "w");
+    /* Write to File */
+    file = fopen("password.txt", "w");
     fputs(password, file);
     fclose(file);
 
