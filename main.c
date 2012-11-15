764774ed46489d1ac29d06153466e6bbcebd1e86
Mika Attila <radiantstatue@gmail.com>
2012-11-15 12:39:28 +0100

    Make legalcharacters a string literal

--- main.c
+++ main.c
@@ -7,9 +7,7 @@ int main()
 {
     srand(time(NULL));
     char password[17];
-    char legalcharacters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z',
-                                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z',
-                                '0','1','2','3','4','5','6','7','8','9', '\0'};
+    const char *legalcharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     int i, num;
     for(i = 0; i < 16; i++)
     {
